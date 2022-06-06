02bab65d57679706f152fbcaa341ad3d65c67d10
Mitch <Mitch.Pauwels@hotmail.com>
2022-06-06 15:59:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
-// Render the emojis as <span> tags in the emojisContainer <div>
-
-// Hints: Use either createElement(), textContent, and append()
-// or innerHTML and a template string
-
+// Make the pop and shift buttons work as well
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
-const emojiContainer = document.getElementById("emojiContainer")
+const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
 
-for (let i = 0; i < myEmojis.length; i++) {
-    // write your code here
-    const emoji = document.createElement('span')
-    emoji.textContent = myEmojis[i]
-    emojiContainer.append(emoji)
+function renderEmojis() {
+    emojiContainer.innerHTML = ""
+    let emoji = ''
+    for (let i = 0; i < myEmojis.length; i++){
+        emoji += `<span>${myEmojis[i]}</span>` 
+    }
+    emojiContainer.innerHTML = emoji
+    emojiInput.value = ''
 }
+renderEmojis()
+
+
+
+
+// function renderEmojis() {
+//     emojiContainer.innerHTML = ""
+//     for (let i = 0; i < myEmojis.length; i++) {
+//         const emoji = document.createElement('span')
+//         emoji.textContent = myEmojis[i]
+//         emojiContainer.append(emoji)
+//     }
+// }
+
+// renderEmojis()
+
+pushBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+unshiftBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+popBtn.addEventListener("click", function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
+shiftBtn.addEventListener("click", function() {
+    myEmojis.shift()
+    renderEmojis()
+})
+
